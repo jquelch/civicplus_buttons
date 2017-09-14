63c706bfcb961da1202cce4e7ec6a5536c7c56a2
jquelch <21263697+jquelch@users.noreply.github.com>
2017-09-14 11:30:12 -0500

    Update server.js
    adding a test for my a button that responds to a webhook in Targetprocess

--- server.js
+++ server.js
@@ -113,6 +113,35 @@ slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
   }
 })
 
+
+
+//lets try to add my button responses here-----------------------
+
+slapp.message('yesno', (msg) => {
+  msg.say({
+      text: '',
+      attachments: [
+        {
+          text: '',
+          fallback: 'Yes or No?',
+          callback_id: 'yesno_callback',
+          actions: [
+            { name: 'answer', text: 'Yes', type: 'button', value: 'yes' },
+            { name: 'answer', text: 'No',  type: 'button',  value: 'no' }
+          ]
+        }]
+      })
+})
+
+slapp.action('yesno_callback', 'answer', (msg, value) => {
+  msg.respond(msg.body.response_url, `${value} is a good choice!`)
+})
+
+
+
+//end of the stuff I added to the demo
+
+
 // attach Slapp to express server
 var server = slapp.attachToExpress(express())
 
